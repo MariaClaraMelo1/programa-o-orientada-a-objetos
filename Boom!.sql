5e1349597d3736b34e3c733d5d741db2fa5b019b
Maria Clara <127156595+MariaClaraMelo1@users.noreply.github.com>
2023-04-24 17:15:23 -0300

    Add files via upload

---
+++ Boom!.sql
@@ -0,0 +1,22 @@
+{inclua biblioteca Util-->u
+	int contador = 10
+	public class()
+	public static void main (String[] args)
+	{ 
+	case(contador >0)
+{	
+		for(int i = 0; i < 100; i++)
+	    System.out.println("Detonação em: " , contador);
+		contador = -1
+		u.aguarde(1000)//Aguarda 1000 milisegundos (1 segundo)
+
+		for(int i = 0; i < 100; i++)
+	    System.out.println("Boom!\n")};
+		
+	}
+}       
+
+
+
+	
+	
